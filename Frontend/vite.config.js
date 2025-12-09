d83ff10c38d8378ab4d54e36f2e2eff60428bfe2
GunterThe <domas.pranaitis@gmail.com>
2025-12-09 19:42:21 +0200

    Changed back

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://backend:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
